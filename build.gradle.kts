7fefa7ed58dc56454a3b6cd84a56815f8e8f4195
PaoPaoYue <846260131@qq.com>
2024-08-04 01:22:00 +0800

    script for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,36 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.register<Copy>("deployToMODFolder") {
+tasks.register<Delete>("cleanTestFolder") {
+    delete(fileTree("E:\\Steam Games\\steamapps\\common\\SlayTheSpire\\mods") { include("*.jar") })
+}
+
+tasks.register<Delete>("cleanPublishFolder") {
+    delete(fileTree("E:\\Steam Games\\steamapps\\common\\SlayTheSpire\\wlj-mod\\content") { include("*.jar") })
+}
+
+tasks.register<Copy>("deployToTestFolder") {
     dependsOn(tasks.clean, tasks.jar)
     from(layout.buildDirectory.dir("libs"))
     include("*.jar")
     into(layout.buildDirectory.dir("E:\\Steam Games\\steamapps\\common\\SlayTheSpire\\mods"))
+}
+
+tasks.register<Copy>("deployToPublishFolder") {
+    dependsOn(tasks.clean, tasks.jar)
+    from(layout.buildDirectory.dir("libs"))
+    include("*.jar")
+    into(layout.buildDirectory.dir("E:\\Steam Games\\steamapps\\common\\SlayTheSpire\\wlj-mod\\content"))
+}
+
+tasks.register<Exec>("runGame") {
+    dependsOn(":cleanTestFolder", ":deployToTestFolder")
+    workingDir("E:\\Steam Games\\steamapps\\common\\SlayTheSpire")
+    commandLine("cmd", "/c", "java -jar mts-launcher.jar")
+}
+
+tasks.register<Exec>("publishMod") {
+    dependsOn(":cleanPublishFolder", ":deployToPublishFolder")
+    workingDir("E:\\Steam Games\\steamapps\\common\\SlayTheSpire")
+    commandLine("cmd", "/c", "java -jar mod-uploader.jar upload -w wlj-mod")
 }
